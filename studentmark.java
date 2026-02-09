b65bda2879aef009a999fa9bac650d718d77c16d
Alex Antony <158134007+A-1-e-x@users.noreply.github.com>
2026-02-09 14:44:48 +0530

    Add files via upload

--- studentmark.java
+++ studentmark.java
@@ -1,22 +1,30 @@
 import java.util.Scanner;
+
 class studentmark
 {
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
+
         System.out.print("Enter number of subjects: ");
         int n = sc.nextInt();
+
         int[] marks = new int[n];
         int total = 0;
+
         for(int i = 0; i < n; i++)
         {
             System.out.print("Enter mark of subject " + (i + 1) + ": ");
             marks[i] = sc.nextInt();
             total += marks[i];
         }
-        double percentage = (double) total / n;
+
+        int maxTotal = n * 100;   
+        double percentage = (double) total / maxTotal * 100;
+
         System.out.println("Total mark: " + total);
         System.out.println("Percentage: " + percentage + "%");
+
         sc.close();
     }
 }
